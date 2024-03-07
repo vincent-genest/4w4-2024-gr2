2266845ca0f6716c7984ce6d475c59c8940013b7
joejoenachos <119699978+joejoenachos@users.noreply.github.com>
2024-03-07 16:34:46 -0500

    s5c2 amélioration du menu et rotation de l'icone

--- header.php
+++ header.php
@@ -17,10 +17,10 @@
     <body>
         <header id="menu" class="global">
             <div class="entete__menu">
-                <label id="burger" for="chk_burger">
-                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7"></path></svg>
-                </label>
                 <input id="chk_burger" type="checkbox" name="">
+                <label id="burger" for="chk_burger">
+                    <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7"></path></svg>
+                </label>
                 <?php wp_nav_menu(array("container" => "nav")); ?>
             </div>
         </header>
