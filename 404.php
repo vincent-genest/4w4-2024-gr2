8ef743f7b978ce9c94108fb400f816e0413aa1f4
joejoenachos <119699978+joejoenachos@users.noreply.github.com>
2024-04-08 12:15:14 -0400

    s9c1 Stylisation de la page 404

--- 404.php
+++ 404.php
@@ -6,14 +6,14 @@
 ?>
 <!-- <h2>index.php</h2> -->
 
-        <div id="accueil" class="global">
-            <section class="accueil__section">
+        <div id="accueil" class="global bck-primaire-100">
+            <section class="accueil__section acceuil__404">
                 <div class="section__cours">
                     <h1>Erreur 404</h1>
                     <p>La page que vous cherchez n'existe pas.</p>
-                    <a href="<?= get_home_url(); ?>">Retourner à l'accueil</a>
-                
+                    <a class="ligne-hover" href="<?= get_home_url(); ?>">Retourner à l'accueil</a>
                 </div>
+                <img class="image-404" src="<?php echo get_template_directory_uri() . '/images/404.png';?>" alt="illustration d'erreur 404">
             </section>
         </div>
 <?php
