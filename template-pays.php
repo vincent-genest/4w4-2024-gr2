4294d9398700bd68ff721412df841da55afd0ffa
joejoenachos <119699978+joejoenachos@users.noreply.github.com>
2024-05-31 15:03:10 -0400

    ef-VG Optimisation du PHP

--- template-pays.php
+++ template-pays.php
@@ -9,25 +9,23 @@
     <section class="accueil__section flexbox">
         <?php
             if ( have_posts() ) : the_post(); ?>
-            <h1><?= get_the_title(); ?></h1>
+                <h1><?= get_the_title(); ?></h1>
             <?php the_content();?>
         <?php endif;?>
     </section>
-
     <?php
         get_template_part('gabarit/vague')
     ?>
 </div>
 
 <div id="pays" class="template global bck-primaire-100">
-    <section class="">
-    <?php
-        // Appel du shortcode directement dans le fichier front-page.php
-        echo do_shortcode('[em_pays]');
-    ?>
+    <section>
+        <?php
+            echo do_shortcode('[em_pays]');
+        ?>
     </section>
-            <?php
-                get_template_part('gabarit/vague')
-            ?>
+    <?php
+        get_template_part('gabarit/vague')
+    ?>
 </div>
 <?php get_footer(); ?>
