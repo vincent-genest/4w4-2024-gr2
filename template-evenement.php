f764d95162b14af401892e416901b51c89a2a276
joejoenachos <119699978+joejoenachos@users.noreply.github.com>
2024-05-31 01:03:10 -0400

    2024-05-31 Correction de la page template pour WHC

--- template-evenement.php
+++ template-evenement.php
@@ -7,11 +7,11 @@
 <?php get_header(); ?>
 <main class="template global">
     <section class="accueil__section flexbox">
-            <?php
-    if ( have_posts() ) : the_post(); ?>
-    <h1><?= get_the_title(); ?></h1>
-    <?php the_content();?>
-    <?php endif;?>
+        <?php
+            if ( have_posts() ) : the_post(); ?>
+            <h1><?= get_the_title(); ?></h1>
+            <?php the_content();?>
+        <?php endif;?>
     </section>
 </main><!-- #main -->
 <?php
